ff2d1ce50e09fa071ea2adbedcef4fc627ecb770
Jacob Lieber <Jlieber124@gmail.com>
2023-04-18 15:01:07 -0400

    --init--.py change

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app.config['MYSQL_DATABASE_PASSWORD'] = 'abc123'
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'animalShelterDB'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'animalShelter'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
